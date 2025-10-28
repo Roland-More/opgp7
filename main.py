419d527569365cfd3286c3ead13d8f9ec0b9bd9d
Roland-More <207071391+Roland-More@users.noreply.github.com>
2025-10-28 08:09:55 +0100

    Uprava modulu tvary.stvorec

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hello, World!")
+from tvary import obdlznik
+print(f"Obvod obdĺžnika o veľkosti 10 x 15 je {obdlznik.obvod(10, 15)}")
---
+++ tvary/stvorec.py
@@ -0,0 +1,7 @@
+from . import obdlznik
+
+def obvod(a):
+    return obdlznik.obvod(a, a)
+
+def obsah(a):
+    return obdlznik.obsah(a, a)
